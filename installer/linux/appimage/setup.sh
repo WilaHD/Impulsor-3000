a69c831fde1d2c020f2511765bfa449bbc2d75a0
Wilhelm Rosinski <w.rosinski@mailbox.org>
2024-05-30 01:21:58 +0200

    Fix AppImage-Directory

--- installer/linux/appimage/setup.sh
+++ installer/linux/appimage/setup.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-APPDIR="Impulsor-3000-x86_64.AppDir"
+APPDIR="Impulsor_3000-x86_64.AppDir"
 
 mkdir -p ${APPDIR}/pdfium/linux-x64
 
@@ -8,4 +8,4 @@ cp ../../../pdfium/linux-x64/libpdfium.so ${APPDIR}/pdfium/linux-x64/libpdfium.s
 cp ../../../target/release/Impulsor-3000 ${APPDIR}/Impulsor-3000
 cp ../../../imgs/logo.png ${APPDIR}/Impulsor-3000.png
 
-cd ${APPDIR} && ln -s Impulsor-3000 AppRun
+cd ${APPDIR} && ln -s ./Impulsor-3000 ./AppRun
