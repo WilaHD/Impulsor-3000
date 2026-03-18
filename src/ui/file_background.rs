a4f0f4eaa6a8235dfbc4d2753f26763359a227f5
Wilhelm <w.rosinski@mailbox.org>
2026-03-18 21:23:23 +0100

    fix(lock): format file lock rs file

--- src/ui/file_background.rs
+++ src/ui/file_background.rs
@@ -1,20 +1,21 @@
 use iced::widget::svg;
 use iced::{widget::container, Alignment, Length::Fill};
 
-use super::Message;
 use super::file_assets::AssetImages;
+use super::Message;
 
 pub fn file_plus() -> container::Container<'static, Message> {
     container(
-container(
-            svg(
-                iced::widget::svg::Handle::from_memory(AssetImages::get("file-plus.svg").unwrap().data)
-            )
-            .width(Fill)
+        container(
+            svg(iced::widget::svg::Handle::from_memory(
+                AssetImages::get("file-plus.svg").unwrap().data,
+            ))
+            .width(Fill),
         )
         .max_height(350)
         .center(Fill)
-        .align_y(Alignment::Center)
+        .align_y(Alignment::Center),
     )
-    .height(Fill).align_y(Alignment::Center)
+    .height(Fill)
+    .align_y(Alignment::Center)
 }
